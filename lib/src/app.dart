ab9196ea57d3465ffa0f3c6e38bc764f81a89f05
Danil Shubin <danil.legioner@gmail.com>
2021-12-20 17:42:11 +0700

    Added dependency firebase with FlutterFire cli.

---
+++ lib/src/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppQuiz extends StatelessWidget {
+  const AppQuiz({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
